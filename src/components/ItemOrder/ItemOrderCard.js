2ffa4cc5d13a3da7b5f7679814ee478f00fb9f59
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-01-15 14:35:29 +0100

    cart page

--- src/components/ItemOrder/ItemOrderCard.js
+++ src/components/ItemOrder/ItemOrderCard.js
@@ -1,16 +1,19 @@
 import React from "react";
-
+import "./ItemOrderCard.scss"
 export default function ItemOrderCard() {
   return (
     <div className="item-order-container">
       <div className="item-detail">
         <div className="img">
-          <img src="" alt="" />
+          <img src="https://bonik-react.vercel.app/assets/images/products/headphone.png" alt="" />
         </div>
+        <div className="name-price">
+
         <h4>Item Name</h4>
         <p className="item-price">
           $250.00 x 2 <span className="unit-total">$500.00</span>
         </p>
+        </div>
       </div>
       <div className="item-card-function">
         <div>close</div>
--- src/pages/ViewCart/ViewCart.js
+++ src/pages/ViewCart/ViewCart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeadFoot from '../../components/HeadFoot'
-import ItemOrderCard from '../../components/ItemOrderCard'
+import ItemOrderCard from '../../components/ItemOrder/ItemOrderCard'
 
 export default function ViewCart() {
   return (
